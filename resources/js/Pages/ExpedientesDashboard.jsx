c2859bb6caf9fc4de87a90554285a581d45b60b8
Kevin <kevinyahirt@gmail.com>
2025-10-17 14:35:16 -0600

    fea:accesos rapidos en los modulos de viajeros y expedientes

--- resources/js/Pages/ExpedientesDashboard.jsx
+++ resources/js/Pages/ExpedientesDashboard.jsx
@@ -54,6 +54,35 @@ export default function Dashboard({auth}) {
                 </ModuleOption>
                 )}
 
+                <ModuleOption managementLink="Reporte de seguimiento de denuncias" href={route('reportes.seguimiento-deununcias')}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6 9 12.75l4.286-4.286a11.948 11.948 0 0 1 4.306 6.43l.776 2.898m0 0 3.182-5.511m-3.182 5.51-5.511-3.181" />
+                    </svg>
+                </ModuleOption>
+
+                <ModuleOption managementLink="Reporte de denuncias por institución" href={route('reportes.denuncias-institucion')}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6 9 12.75l4.286-4.286a11.948 11.948 0 0 1 4.306 6.43l.776 2.898m0 0 3.182-5.511m-3.182 5.51-5.511-3.181" />
+                    </svg>
+                </ModuleOption>
+
+                <ModuleOption managementLink="Reporte de documentos faltantes por expediente" href={route('reportes.documentos-faltantes')}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6 9 12.75l4.286-4.286a11.948 11.948 0 0 1 4.306 6.43l.776 2.898m0 0 3.182-5.511m-3.182 5.51-5.511-3.181" />
+                    </svg>
+                </ModuleOption>
+
+                <ModuleOption managementLink="Reporte de expedientes completos" href={route('reportes.expedientes-completos')}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6 9 12.75l4.286-4.286a11.948 11.948 0 0 1 4.306 6.43l.776 2.898m0 0 3.182-5.511m-3.182 5.51-5.511-3.181" />
+                    </svg>
+                </ModuleOption>
+
+                <ModuleOption managementLink="Reporte de servidores omisos" href={route('reportes.servidores-omisos')}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6 9 12.75l4.286-4.286a11.948 11.948 0 0 1 4.306 6.43l.776 2.898m0 0 3.182-5.511m-3.182 5.51-5.511-3.181" />
+                    </svg>
+                </ModuleOption>
             </div>
         </MainLayout>
     );
